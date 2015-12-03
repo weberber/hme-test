625a1a050c502ff237cfa7bb30ac764488f6ca3c
Holan <holan@aquariumlightings.com>
2015-12-03 16:25:19 +0800

    Try 485 A to B

--- python01.py
+++ python01.py
@@ -2,20 +2,31 @@ import serial
 import time
 
 ser = serial.Serial()
+ser2 = serial.Serial()
 
 ser.port = "COM16"
 ser.baudrate = 115200
 
-ser.open()
+ser2.port = "COM17"
+ser2.baudrate = 115200
 
-print (ser.isOpen())
+ser.open()
+ser2.open()
+
+if ser.isOpen():
+        print ("COM16 is OK")
+        
+if ser2.isOpen():
+        print ("COM17 is OK")
+
+#print (ser.isOpen())
+
 
 x = 0xf0
-tstlist=[0x80,0x01,0x00,0x00,0x32,0x01,
-         0x00,0x00,0x05,0x00,0x00,0x01,0x00,0x00,0x3A,0x01,0x00]
+tstlist=[0x80,0x01,0x02,0x03]
 
 
-print ( tstlist[3:8] )
+print ( tstlist[0:3] )
 
 if ser.isOpen():
 
@@ -27,8 +38,10 @@ if ser.isOpen():
 
         tStart = time.time()
         while( 50 > tStart - time.time() ):
-                print ( ser.read(1) )
+                print ( ser2.read(2) )
 
         
 
 ser.close()
+ser2.close()
+
