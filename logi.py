7e4c1dbd8492b4a385c0a7be01387dd437dafe1b
Holan <holan@aquariumlightings.com>
2015-12-04 14:48:54 +0800

    logi.py 8B[2] 8B[0] OK

--- logi.py
+++ logi.py
@@ -4,19 +4,39 @@ import struct
 
 def WordTo3Byte():
 
-    u16word = 0xf0ff
+    u16word = 0xfff5
+    
     
     u8Byte = [0x00,0x00,0x00]
 
     
     u16Data = struct.pack("H", u16word)
    
-    u8Byte0, u8Byte1 =  struct.unpack("2B", u16Data)
     # s to 2 B (byte)element
+    u16wByte0, u16wByte1 =  struct.unpack("2B", u16Data)
 
-    print (u8Byte0)
-    print (u8Byte1)
-    print (1)
+    #move to u8B[2]    ok
+    u8Byte[2] = u16wByte1 >> 6
+
+    #move to u8B[0] ok
+    print (hex(u16wByte0))
+    print ( hex(u16wByte0 & 0x7f ) )
+            
+    #move to u8B[1]
+            
+    u16word = ( u16word << 1) & 0x7f00
+    u16Data = struct.pack("H", u16word)
+    print ( u16Data )
+    u16wByte0, u16wByte1 =  struct.unpack("2B", u16Data)
+    u8Byte[1] = u16wByte0
+    
+ 
+    print (u8Byte[2])
+    print (u8Byte[1])
+    print (hex(u8Byte[2]) )  
+    print (hex(u8Byte[1]) )
+    print (hex(u8Byte[0]) )
+    #bin hex
 
 
 if __name__ == '__main__':
